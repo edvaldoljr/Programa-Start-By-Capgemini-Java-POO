dca734401d90b0f230114f4bff2c73d5ff85f21c
edvaldojr <edvaldoluizj@gmail.com>
2022-09-15 04:45:46 -0300

    JAVA POO

---
+++ Classes/app/src/main/java/Classes/App.java
@@ -0,0 +1,21 @@
+
+package Classes;
+
+import java.util.Scanner;
+
+public class App {
+   public static void main (String[] args) {
+       
+       Scanner scan = new Scanner(System.in);
+       
+       Pessoa objetoPessoa = new Pessoa(70.0f, 1.58f);
+       
+//       System.out.println("Digite o peso da pessoa");
+//       objetoPessoa.setPeso(scan.nextFloat());
+//       
+//       System.out.println("Digitte a alttura da pessoa");
+//       objetoPessoa.setAltura(scan.nextFloat()) ;
+       
+       System.out.println("IMC: " + objetoPessoa.culcularIMC());
+   }
+}
---
+++ Classes/app/src/main/java/Classes/Pessoa.java
@@ -0,0 +1,40 @@
+package Classes;
+
+/**
+ *
+ * @author Edvaldojr
+ */
+
+public class Pessoa {
+    
+    private float peso;
+    private float altura;
+    
+    public Pessoa(float peso, float altura){
+        this.peso = peso;
+        this.altura = altura;
+    }
+    
+    public float culcularIMC (){
+        float imc = peso / (altura * altura);
+        return imc;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    
+}
