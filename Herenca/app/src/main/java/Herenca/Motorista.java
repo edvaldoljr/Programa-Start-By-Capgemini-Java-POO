3067d3c09acdb35af56577c3cf55f4e29530fb8e
edvaldojr <edvaldoluizj@gmail.com>
2022-10-05 18:57:53 -0300

    Herança aula 2

--- Herenca/app/src/main/java/Herenca/Motorista.java
+++ Herenca/app/src/main/java/Herenca/Motorista.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Herenca;
 
-/**
- *
- * @author Edvaldojr
- */
-public class Motorista {
+public class Motorista extends Funcionario  {
+    
+ 
+    public Motorista() {
+        
+    }
     
 }
--- Herenca/app/src/main/java/Herenca/Vendedor.java
+++ Herenca/app/src/main/java/Herenca/Vendedor.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Herenca;
 
-/**
- *
- * @author Edvaldojr
- */
-public class Vendedor {
+public class Vendedor extends Funcionario {
+    
+   
+    private int TotalItensVendidos;
+    private float comissaoPorItem;
+    
+    public Vendedor(){
+        super();
+    }
+
+    public int getTotalItensVendidos() {
+        return TotalItensVendidos;
+    }
+
+    public void setTotalItensVendidos(int TotalItensVendidos) {
+        this.TotalItensVendidos = TotalItensVendidos;
+    }
+
+    public float getComissaoPorItem() {
+        return comissaoPorItem;
+    }
+
+    public void setComissaoPorItem(float comissaoPorItem) {
+        this.comissaoPorItem = comissaoPorItem;
+    }
+    
     
 }
